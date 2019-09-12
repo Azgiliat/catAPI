994314ff6aef9f89a81cd726c9a8ab800df5fab1
Anton <shapovalov@uchi.ru>
2019-09-12 17:50:45 +0300

    kitty.v1.1

--- webpack.common.js
+++ webpack.common.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   output: {
     filename: `bundle.js`,
-    path: path.join(__dirname, `public`),
+    path: path.join(__dirname, `docs`),
   },
   plugins: [
     new HtmlWebpackPlugin({
